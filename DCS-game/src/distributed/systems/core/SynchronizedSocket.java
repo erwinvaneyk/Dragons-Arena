a7e783127478ecc98aad60b39b6111782ad90479
erwinvaneyk <erwinvaneyk@gmail.com>
2015-03-10 15:47:25 +0100

    Implement synchronizedsocket-wrapper methods

--- DCS-game/src/distributed/systems/core/SynchronizedSocket.java
+++ DCS-game/src/distributed/systems/core/SynchronizedSocket.java
@@ -3,34 +3,36 @@ package distributed.systems.core;
 import distributed.systems.das.BattleField;
 import distributed.systems.das.units.Unit;
 
-public class SynchronizedSocket implements Socket{
+public class SynchronizedSocket implements Socket {
+
+	private final Socket local;
 
 	public SynchronizedSocket(Socket local) {
-		// TODO Auto-generated constructor stub
+		this.local = local;
 	}
 
 	@Override
 	public void register(String serverid) {
-
+		this.local.register(serverid);
 	}
 
 	@Override
 	public void addMessageReceivedHandler(BattleField battleField) {
-
+		this.local.addMessageReceivedHandler(battleField);
 	}
 
 	@Override
 	public void addMessageReceivedHandler(Unit unit) {
-
+		this.local.addMessageReceivedHandler(unit);
 	}
 
 	@Override
 	public void sendMessage(Message reply, String origin) {
-
+		this.local.sendMessage(reply, origin);
 	}
 
 	@Override
 	public void unRegister() {
-
+		this.local.unRegister();
 	}
 }
